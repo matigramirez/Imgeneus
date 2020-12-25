bb1a5e62105c3c6116f295172a6110ad2784a525
Matias Ramirez <matigramirez@gmail.com>
2020-12-25 12:28:44 -0300

    Configuration for maximum level and stat/skill points per level

---
+++ src/Imgeneus.World/Game/Player/DefaultLevelStatSkillPoints.cs
@@ -0,0 +1,11 @@
+using Imgeneus.Database.Entities;
+
+namespace Imgeneus.World.Game.Player
+{
+    public class DefaultLevelStatSkillPoints
+    {
+        public Mode Mode { get; set; }
+        public byte StatPoint { get; set; }
+        public byte SkillPoint { get; set; }
+    }
+}
